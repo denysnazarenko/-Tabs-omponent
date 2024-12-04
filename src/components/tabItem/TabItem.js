320c9121e634ac6bcb2bab7274567b3083543479
DenysNazarenko <dinis.nazarenko03@gmail.com>
2024-12-04 14:41:58 +0200

    updated DnD

--- src/components/tabItem/TabItem.js
+++ src/components/tabItem/TabItem.js
@@ -1,3 +1,4 @@
+import { useState, useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { Reorder } from 'framer-motion';
 import { setActiveTab } from '../tabContainer/TabsSlice';
@@ -5,17 +6,42 @@ import { setActiveTab } from '../tabContainer/TabsSlice';
 import './tabItem.scss';
 
 const TabItem = ({ tab }) => {
+  const [timer, setTimer] = useState(null);
+  const [isDraggable, setIsDraggable] = useState(window.innerWidth >= 767);
   const dispatch = useDispatch();
 
   const setActive = () => {
     dispatch(setActiveTab(tab.id))
   }
 
+  const handlePointerDown = () => {
+    if (window.innerWidth < 767) {
+      const newTimer = setTimeout(() => {
+        setIsDraggable(true);
+      }, 2000);
+      setTimer(newTimer);
+    } else {
+      setIsDraggable(true);
+    }
+  };
+
+  const handlePointerUp = () => {
+    clearTimeout(timer);
+    setIsDraggable(window.innerWidth >= 767);
+  };
+
   return (
     <Reorder.Item
       value={tab}
       className={`tab-item ${tab.isPinned ? "pinned" : ""} ${tab.isActive ? "active" : ""}`}
-      onClick={setActive}>
+      onClick={setActive}
+      onPointerDown={handlePointerDown}
+      onPointerUp={handlePointerUp}
+      drag={isDraggable}
+      whileDrag={{
+        background: "#7f858d",
+        color: "#fff",
+      }}>
       {tab.title}
     </Reorder.Item>
   );
