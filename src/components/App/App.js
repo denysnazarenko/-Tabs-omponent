b2b1642104e2be3fffdd4a6d7773a32bf9618d88
DenysNazarenko <dinis.nazarenko03@gmail.com>
2024-12-04 10:21:37 +0200

    first commit

---
+++ src/components/App/App.js
@@ -0,0 +1,11 @@
+import '../../style/index.scss';
+
+const App = () => {
+  return (
+    <main className="app">
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Rerum doloribus deserunt quod. Velit laboriosam iusto adipisci neque quidem, nisi eveniet corporis, exercitationem odit suscipit dolorum veniam harum autem tenetur voluptatum.
+    </main>
+  )
+}
+
+export default App;
